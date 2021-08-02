bbfd7bd9717c3483064e2de45644e5aeda590560
Okola44 <mercyimaliokola@gmail.com>
2021-08-02 07:59:50 +0300

    changes

--- school_system/student/forms.py
+++ school_system/student/forms.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.forms import fields
 from .models import Student
 
+
 class StudentRegistrationForms(forms.ModelForm):
     class Meta:
         model=Student
--- school_system/student/models.py
+++ school_system/student/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.enums import Choices
+from django.db.models.fields import CharField, PositiveSmallIntegerField
 
 
 # Create your models here.
@@ -8,26 +9,32 @@ from django.db.models.enums import Choices
 class Student(models.Model):
     first_name=models.CharField(max_length=10)
     last_name=models.CharField(max_length=10)
-    EmailAdress=models.EmailField()
-    DOB=models.DateField()
-    age=models.PositiveSmallIntegerField()
-    CountyorDistrict=models.CharField(max_length=20)
-    National_ID=models.CharField(max_length=20)
-    Student_ID=models.PositiveSmallIntegerField()
-    language_choice=((u'E',u'English'),
-                      (u'K',u'Kinyarwanda'),
-                      (u'K',u'Kiswahili'),)
-    language=models.CharField(max_length=2,choices=language_choice)
-    Date_Of_Enrollment=models.DateField()
-    Medical_Report=models.FileField()
-    # ourse_name=models.ManyToManyField(Courses)
-    Profile=models.ImageField(upload_to='head_shots')
-    Serial_Number=models.CharField(max_length=15)
-    Roll_number=models.PositiveSmallIntegerField()
     gender_choices=((u'M',u'male'),
     (u'F',u'female'),
     (u'O',u'Other'),)
     gender=models.CharField(max_length=1,choices=gender_choices)
+    emailAdress=models.EmailField()
+    # Phone_number=models.phone
+    dob=models.DateField()
+    gurdians_name=models.CharField(max_length=20)
+    age=models.PositiveSmallIntegerField()
+    countyorDistrict=models.CharField(max_length=20)
+    national_ID=models.CharField(max_length=20)
+    student_ID=models.PositiveSmallIntegerField()
+    language_choice=((u'E',u'English'),
+                      (u'K',u'Kinyarwanda'),
+                      (u'K',u'Kiswahili'),)
+    language=models.CharField(max_length=2,choices=language_choice)
+    date_of_enrollment=models.DateField()
+    medical_Report=models.FileField()
+    # ourse_name=models.ManyToManyField(Courses)
+    profile=models.ImageField(upload_to='head_shots')
+    serial_Number=models.CharField(max_length=15)
+    roll_number=models.PositiveSmallIntegerField()
+ 
+    laptop_number=models.CharField(max_length=10)
+    grade=models.CharField(max_length=5)
+
 
 
 
